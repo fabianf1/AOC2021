c62ef1e39ce440984756ef6b7cab88a4606d8b4d
Fabian Fool <f.fool@tudelft.nl>
2021-12-08 19:25:31 +0100

    Day 8 - Nicer method

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,112 +1,38 @@
-from collections import defaultdict
-from typing import Tuple
-
 def load_data(path: str) -> list:
-    return [[input.split() for input in line.split(' | ')] for line in open(path).read().strip().splitlines()]
-
-
-def count_unique_numbers_in_output(input: list) -> int:
-    count = 0
-    for line in input:
-        for number in line[1]:
-            if len(number) != 5 and len(number) != 6:
-                count += 1
-    return count
-
-
-def decode_connections(input: list) -> list:
-# seg_map
-#     000
-#    1   2
-#    1   2
-#     333
-#    4   5
-#    4   5
-#     666
-#
-    num_idx = [-1] * 10
-    seg_map = [-1] * 7
-    # Find indices of 1, 4, 7 and 8
-    idx235 = []
-    idx069 = []
-    for i in range(10):
-        if len(input[i]) == 2:
-            num_idx[1] = i
-        elif len(input[i]) == 4:
-            num_idx[4] = i
-        elif len(input[i]) == 3:
-            num_idx[7] = i
-        elif len(input[i]) == 7:
-            num_idx[8] = i
-        elif len(input[i]) == 5:
-            idx235.append(i)
-        elif len(input[i]) == 6:
-            idx069.append(i)
-    # Distinguish 1,3 and 4,6
-    char13 = []
-    char46 = []
-    for char in input[num_idx[8]]:
-        if char in input[num_idx[7]]:
-            continue
-        if char not in input[num_idx[4]]:
-            char46.append(char)
-        else:
-            char13.append(char)
-    # Find bottom-left and the number2 using 2,3 and 5
-    for idx in idx235:
-        if char46[0] in input[idx] and char46[1] in input[idx]:
-            num_idx[2] = idx
-            idx235.remove(idx)
-            if char46[0] not in input[idx235[0]]:
-                seg_map[4] = char46[0]
-            else:
-                seg_map[4] = char46[1]
-            break
-    # Distinguish top-left and middle, and separate 3 and 5
-    for idx in idx235:
-        if len(idx235) == 1:
-            break
-        for char in char13:
-            if char not in input[idx]:
-                # Number
-                num_idx[3] = idx
-                idx235.remove(idx)
-                num_idx[5] = idx235[0]
-                # Mapping
-                char13.remove(char)
-                seg_map[3] = char13[0]
-                break
-    # Distinguish 0, 6, 9
-    for idx in idx069:
-        if seg_map[4] not in input[idx]:
-            num_idx[9] = idx
-        elif seg_map[3] not in input[idx]:
-            num_idx[0] = idx
-        else:
-            num_idx[6] = idx
-    # Found all numbers -> Sort mapping and return
     output = []
-    for i in range(10):
-        temp = list(input[num_idx[i]])
-        temp.sort()
-        output.append(temp)
-
+    for x, y in [x.split('|') for x in open(path).read().strip().splitlines()]:
+        # This creates a dictionary; Key corresponds to the length;
+        # Because only 1,4,7,8 have a unique length they are the only ones certain to be in here
+        output.append([{len(s): set(s) for s in x.split()}, y.split()])
     return output
 
 
-def sum_output(input: list) -> int:
+def count_unique_numbers_in_output(data: list) -> int:
     count = 0
-    for line in input:
-        decoder = decode_connections(line[0])
-        num = []
-        for number in line[1]:
-            number = list(number)
-            number.sort()
-            num.append(decoder.index(number))
-        # Number
-        count += int(''.join(map(str,num)))
-
+    for line in data:
+        count += len([len(x) for x in line[1] if len(x) not in [5,6]])
     return count
+
+
+def sum_output_numbers(data: list) -> int:
+    c = 0
+    for l in data:
+        n = ''
+        for o in map(set, l[1]): # Map transforms every entry in l[1] into a set
+            match len(o), len(o&l[0][4]), len(o&l[0][2]): # Differentiate between numbers using length, overlap with 4 and overlap with 1
+                case 2, _, _: n += '1'
+                case 3, _, _: n += '7'
+                case 4, _, _: n += '4'
+                case 7, _, _: n += '8'
+                case 5, 2, _: n += '2'
+                case 5, 3, 1: n += '5'
+                case 5, 3, 2: n += '3'
+                case 6, 4, _: n += '9'
+                case 6, 3, 1: n += '6'
+                case 6, 3, 2: n += '0'
+        c += int(n)
+    return c
+
 
 def main() -> None:
     # Load data
@@ -116,8 +42,8 @@ def main() -> None:
     assert count_unique_numbers_in_output(test) == 26
     print(count_unique_numbers_in_output(data))
     # Part 2
-    assert sum_output(test) == 61229
-    print(sum_output(data))
+    assert sum_output_numbers(test) == 61229
+    print(sum_output_numbers(data))
 
 
 if __name__ == "__main__":
