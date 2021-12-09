e95f7633c240e5747ebcf5f5d7a7416625a7b3c3
Fabian Fool <f.fool@tudelft.nl>
2021-12-09 20:45:46 +0100

    Day 9 - Cleanup

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -1,13 +1,6 @@
 def load_and_prepare_data(path: str) -> list:
-    data = []
-    for line in open(path).read().strip().splitlines():
-        line = [int(val) for val in list(line)]
-        line.append(9)
-        line.insert(0,9)
-        data.append(line)
-    data.append([9] * len(data[0]))
-    data.insert(0,  [9] * len(data[0]))
-    return data
+    data = [[int(val) for val in list('9' + line + '9')] for line in open(path).read().strip().splitlines()]
+    return [[9] * len(data[0])] + data + [[9] * len(data[0])]
 
 def find_min_pos(d: list) -> list:
     min_pos = []
@@ -19,29 +12,23 @@ def find_min_pos(d: list) -> list:
 
 def calc_risk_sum(d: list) -> int:
     count = 0
-    for p in find_min_pos(d):
-        count += 1 + d[p[0]][p[1]]
+    for i, j in find_min_pos(d):
+        count += 1 + d[i][j]
     return count
 
 def extend_basin(d:list, i: int, j:int, s: set) -> set:
-    if d[i][j] == 9:
-        return s
-    s.add(tuple([i,j]))
-    if tuple([i + 1, j]) not in s:
-        s = s.union(extend_basin(d, i + 1, j, s))
-    if tuple([i - 1, j]) not in s:
-        s = s.union(extend_basin(d, i - 1, j, s))
-    if tuple([i, j + 1]) not in s:
-        s = s.union(extend_basin(d, i, j + 1, s))
-    if tuple([i, j - 1]) not in s:
-        s = s.union(extend_basin(d, i, j - 1, s))
+    if d[i][j] != 9:
+        s.add((i,j))
+        if (i + 1, j) not in s: s = s | extend_basin(d, i + 1, j, s)
+        if (i - 1, j) not in s: s = s | extend_basin(d, i - 1, j, s)
+        if (i, j + 1) not in s: s = s | extend_basin(d, i, j + 1, s)
+        if (i, j - 1) not in s: s = s | extend_basin(d, i, j - 1, s)
     return s
 
 def basin_size(d: list) ->  int:
-    min_pos = find_min_pos(d)
     sz_list = []
-    for p in min_pos:
-        sz_list.append(len(extend_basin(d, p[0], p[1], set())))
+    for i, j in find_min_pos(d):
+        sz_list.append(len(extend_basin(d, i, j, set())))
     sz_list.sort(reverse=True)
     return sz_list[0] * sz_list[1] * sz_list[2]
 
@@ -51,11 +38,10 @@ def main() -> None:
     data = load_and_prepare_data('data.txt')
     # Part 1
     assert calc_risk_sum(test) == 15
-    print('Part 1:' + str(calc_risk_sum(data)))
+    print('Part 1: ' + str(calc_risk_sum(data)))
     # Part 2
     assert basin_size(test) == 1134
     print('Part 2: ' + str(basin_size(data)))
-
 
 if __name__ == "__main__":
     main()
